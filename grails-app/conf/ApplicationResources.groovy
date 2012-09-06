51f1f2bd0acab1523ec69b7f64ebf3427d911a50
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-09-06 18:01:33 -0300

    Added bundle functionality

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -15,14 +15,23 @@ modules = {
     wizard {
       dependsOn 'jquery'
       dependsOn 'fileuploader'
-      resource url:'js/jquery/jquery.validationEngine-pt.js'
-      resource url:'js/jquery/jquery.validationEngine.js'
-      resource url:'js/jquery/validationEngine.jquery.css'
-      resource url:'js/jquery/jquery.maskedinput-1.2.2.min.js'
-      resource url:'js/jquery/jquery.smartWizard-2.0.min.js'
-      resource url:'js/bootstrap/bootstrap.min.js'
-      resource url:'js/inscricoes/index.js'
-      resource url:'css/wizard/smart_wizard.css'
-      resource url:'css/bootstrap/bootstrap.min.css'
+      resource url:'js/jquery/jquery.validationEngine-pt.js', bundle: 'wizard'
+      resource url:'js/jquery/jquery.validationEngine.js', bundle: 'wizard'
+      resource url:'js/jquery/validationEngine.jquery.css', bundle: 'wizard'
+      resource url:'js/jquery/jquery.maskedinput-1.2.2.min.js', bundle: 'wizard'
+      resource url:'js/jquery/jquery.smartWizard-2.0.min.js', bundle: 'wizard'
+      resource url:'js/bootstrap/bootstrap.min.js', bundle: 'wizard'
+      resource url:'js/inscricoes/index.js', bundle: 'wizard'
+      resource url:'css/wizard/smart_wizard.css', bundle: 'wizard'
+      resource url:'css/bootstrap/bootstrap.min.css', bundle: 'wizard'
+    }
+    
+    overrides {
+      jquery {
+        defaultBundle 'wizard'
+      }
+      fileuploader {
+        defaultBundle 'wizard'
+      }
     }
 }
---
+++ grails-app/views/inscricoes/_modalResumo.gsp
@@ -0,0 +1,24 @@
+<div id="modal_resumo" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Confirme as informações abaixo</h3>
+  </div>
+  <div class="modal-body">
+    <p>
+      <h4>Clique no botão abaixo para enviar o seu comprovante de anuidade da ${tipoSocio.toUpperCase()}</h4>
+      <uploader:uploader id="${tipoSocio}_upload" allowedExtensions="${"['png', 'jpg', 'jpeg']"}">
+        <uploader:onProgress>console.log(loaded + ' of ' + total + ' done so far') </uploader:onProgress>
+        <uploader:onComplete>
+          fezUpload${tipoSocio.capitalize()} = true;
+          $("#modal_${tipoSocio}").modal('hide');
+          $('#div_socio').html(criarAlerta("Sucesso!", "Seu arquivo foi enviado com sucesso !"));
+          exibirTotal();
+        </uploader:onComplete>
+        <uploader:onCancel>alert('Você cancelou o envio do arquivo'); </uploader:onCancel>
+        <uploader:noScript>
+          <p>O seu navegador não suporta a execução de scripts</p>
+        </uploader:noScript>
+      </uploader:uploader>
+    </p>
+  </div>
+</div>  
