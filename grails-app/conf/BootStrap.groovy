f3894cdcfb54949beba4a67c873c0a0ea8d0103d
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-09-13 07:29:25 -0300

    Adicionada a mudança no nome do minicurso e no digito verificador da conta bancaria

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -26,6 +26,9 @@ class BootStrap {
     def miniCurso2 = MiniCurso.findByIdentificador(2) ?: new MiniCurso(identificador: 2, titulo: 'A citogenética molecular aplicada ao diagnóstico pré-natal e às perdas fetais', professor: 'Elen Rose Carvalho - (Códon Biotecnologia - BH)', vagas: 70, valor: 20.0).save(failOnError: true)
     def miniCurso3 = MiniCurso.findByIdentificador(3) ?: new MiniCurso(identificador: 3, titulo: 'Conceitos e aplicações da medicina nuclear no diagnóstico em oncologia', professor: 'Marcelo César Garcia Carneiro - (Hospital Pró-Cardíaco RJ)', vagas: 70, valor: 20.0).save(failOnError: true)
     def miniCurso4 = MiniCurso.findByIdentificador(4) ?: new MiniCurso(identificador: 4, titulo: 'Teste de paternidade: técnicas básicas e cálculos estatísticos', professor: 'Juliana Ramos Pimenta - (Síntese Biotecnologia-BH)', vagas: 70, valor: 20.0).save(failOnError: true)
+    
+    miniCurso3.titulo = 'Medicina Nuclear - princípios e aplicações'
+    miniCurso3.save(flush: true, failOnError: true)
   }
   
   def destroy = {
